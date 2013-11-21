3d53482134f30fb47eaf5f72e99f8900f967d298
romanbucnev <roman.bucnev@googlemail.com>
2013-11-21 20:16:32 +0100

    Klasse Node: addNode(), isVisited(), visit(), unvisited() eingefügt.

---
+++ Generics/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/TestMain.java
@@ -0,0 +1,17 @@
+package de.hs_ma.ws_13.tpe_04_uib.auer_baummann_bucnev.generics;
+
+import java.util.ArrayList;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		String bier = new String();
+		
+		ArrayList haha = new ArrayList();
+		
+		
+
+	}
+
+}
