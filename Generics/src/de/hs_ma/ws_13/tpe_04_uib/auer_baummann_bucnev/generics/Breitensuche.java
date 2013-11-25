f98e7430205c311a70099f30dc835723efdd28d9
baumanntobias <tobias_baumann1@gmx.net>
2013-11-25 12:16:13 +0100

    sxscv

---
+++ Generics/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Breitensuche.java
@@ -0,0 +1,13 @@
+package de.hs_ma.ws_13.tpe_04_uib.auer_baummann_bucnev.generics;
+
+public abstract class Breitensuche implements SearchStrategy{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Generics/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Tiefensuche.java
@@ -0,0 +1,25 @@
+package de.hs_ma.ws_13.tpe_04_uib.auer_baummann_bucnev.generics;
+
+public class Tiefensuche implements SearchStrategy{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public <T> String search(T wert, boolean breitensuche) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
