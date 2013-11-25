6d05fe2e8fe80c993dc76127994014bd68044189
baumanntobias <tobias_baumann1@gmx.net>
2013-11-25 12:13:26 +0100

    2 neue Klassen ListImpl serial nummer dazugefüügt

--- Generics/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/ListImpl.java
+++ Generics/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/ListImpl.java
@@ -13,5 +13,6 @@ import java.util.LinkedList;
  *            Typ der gespeicherten Elemente
  */
 public abstract class ListImpl<T> extends LinkedList<T> implements List<T> {
-
+	
+	private static final long serialVersionUID = 3130055300544467423L;
 }
