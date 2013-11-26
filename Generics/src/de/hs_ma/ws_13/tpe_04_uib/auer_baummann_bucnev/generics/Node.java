ad5a9c721cd898c54605c2cc4329141bb209821d
Patric Auer <patric.auer@googlemail.com>
2013-11-26 17:52:16 +0100

    JavaDoc in der Klasse Node hinzugefügt

--- Generics/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Node.java
+++ Generics/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Node.java
@@ -16,10 +16,12 @@ public class Node<T> {
 	private NodeListImpl<T> childList;
 
 	/**
-	 * Konstruktor
+	 * Konstruktor, zur Erzeugung eines Knoten
 	 * 
 	 * @param nodeName
+	 *            Name des Knoten
 	 * @param wert
+	 *            den der Knoten enthalten soll
 	 */
 	public Node(String nodeName, T value) {
 		this.nodeName = nodeName;
@@ -28,28 +30,58 @@ public class Node<T> {
 
 	}
 
+	/**
+	 * Methode, um Kindeknoten hinzuzufügen
+	 * 
+	 * @param child
+	 *            Kindknoten
+	 */
 	void addChild(Node<T> child) {
 		this.childList.add(child);
 	}
 
-	// Auslesen aller Kindsknoten
+	/**
+	 * Methode zum Auslesen aller Kindknoten
+	 * 
+	 * @return childList Liste aller Kindknoten
+	 */
 	NodeListImpl<T> getChildren() {
 		return this.childList;
 	}
 
+	/**
+	 * Methode um den Namen zu bekommen
+	 * 
+	 * @return nodeName Name des Knoten
+	 */
 	String getName() {
 		return this.nodeName;
 	}
 
-	// Auslesen des Wertes
+	/**
+	 * Methode zum Auslesen der Werte
+	 * 
+	 * @return value enthaltener Wert
+	 */
 	T getValue() {
 		return this.value;
 	}
 
+	/**
+	 * Methode, um zu prüfen, ob ein Knoten bereits besucht wurde
+	 * 
+	 * @return visit
+	 */
 	public boolean isVisited() {
 		return this.visit();
 	}
 
+	/**
+	 * Methode, um festzulegen, ob ein Knoten besucht oder nicht besucht wurde
+	 * 
+	 * @return true wenn der Knoten besucht wurde; false wenn der Knoten nocht
+	 *         nicht besucht wurde
+	 */
 	public boolean visit() {
 		if (isVisited) {
 			return isVisited = true;
@@ -61,7 +93,9 @@ public class Node<T> {
 	// isVisited = false;
 	// }
 
-	@Override
+	/**
+	 * Methode, um eine geeignete Darstellung zu erzeugen
+	 */
 	public String toString() {
 		return "Node [nodeName=" + nodeName + ", value=" + value
 				+ ", childList=" + childList + "]";
