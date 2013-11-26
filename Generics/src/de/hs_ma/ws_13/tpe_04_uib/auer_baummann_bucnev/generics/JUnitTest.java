baf36bdd3e3d07da0cc3e93ef6451a5c41611acf
romanbucnev <roman.bucnev@googlemail.com>
2013-11-26 23:42:23 +0100

    JUnit begonnen

--- Generics/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/JUnitTest.java
+++ Generics/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/JUnitTest.java
@@ -17,7 +17,47 @@ public class JUnitTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
-	}
+//		Breitensuche<String> breitensuche = new Breitensuche<String>();
+		Tiefensuche<String> tiefensuche = new Tiefensuche<String>();
 
+		Node<String> a = new Node<String>("A", "A");
+		Node<String> b = new Node<String>("B", "B");
+		Node<String> c = new Node<String>("C", "C");
+		Node<String> d = new Node<String>("D", "D");
+		Node<String> e = new Node<String>("E", "E");
+		Node<String> f = new Node<String>("F", "F");
+		Node<String> g = new Node<String>("G", "G");
+		Node<String> h = new Node<String>("H", "H");
+		Node<String> i = new Node<String>("I", "I");
+		Node<String> j = new Node<String>("J", "J");
+		Node<String> k = new Node<String>("K", "K");
+		Node<String> l = new Node<String>("L", "L");
+		Node<String> m = new Node<String>("M", "M");
+		Node<String> n = new Node<String>("N", "N");
+		Node<String> o = new Node<String>("O", "O");
+		Node<String> p = new Node<String>("P", "P");
+
+		a.addChild(d);
+		a.addChild(c);
+		a.addChild(b);
+		b.addChild(g);
+		b.addChild(f);
+		b.addChild(e);
+		b.addChild(a);
+		g.addChild(d);
+		f.addChild(k);
+		f.addChild(j);
+		e.addChild(h);
+		e.addChild(i);
+		k.addChild(d);
+		j.addChild(i);
+		h.addChild(b);
+
+		Graph<String> firstGraph = new Graph<String>(a);
+		
+
+		assertEquals("K",firstGraph.search(k,new Breitensuche<String>()));
+		assertEquals("A, D, C, B, G, F, E, K, J, H, I", new Breitensuche<String>().getPath());
+
+	}
 }
