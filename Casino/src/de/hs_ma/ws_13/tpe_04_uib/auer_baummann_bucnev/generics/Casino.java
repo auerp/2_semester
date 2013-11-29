43b774e3d5592a75eb6ae78b98dcde8124212c3e
Patric Auer <patric.auer@googlemail.com>
2013-11-29 11:13:55 +0100

    diverse Änderungen_2

--- Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Casino.java
+++ Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Casino.java
@@ -2,32 +2,10 @@ package de.hs_ma.ws_13.tpe_04_uib.auer_baummann_bucnev.generics;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Collections.*;
-
 
 public class Casino {
 
 	private int vermoegen;
+	private ArrayList Tische;
 
-	// Innere Klasse
-	private class Dealer{
-		
-		// ArrayList für das Kartendeck
-		private ArrayList arrayListKartenDeck = new ArrayList();
-		
-		//Methoden
-		private  mischen(ArrayList arrayListKartenDeck){
-			return shuffle();
-		}
-		
-		private ... austeilen(){
-			
-		}
-		
-		// mischt das Kartendeck
-		protected ArrayList shuffle(ArrayList arrayListKartenDeck){
-				Collections.shuffle(arrayListKartenDeck);
-			
-		}
-	}
 }
--- Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Tische.java
+++ Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Tische.java
@@ -1,7 +1,22 @@
 package de.hs_ma.ws_13.tpe_04_uib.auer_baummann_bucnev.generics;
 
+import java.util.Collections;
+import java.util.Stack;
+
 public class Tische extends Casino {
 
-	
-	
+	class Dealer {
+		// Methoden
+		protected void mischen(Stack kartendeck) {
+			Collections.shuffle(kartendeck);
+		}
+
+		protected void austeilen(Stack kartendeck) {
+			// Wie viele Spieler?
+			// 3 Karten pro Spieler
+			// Dealer spielt mit
+		}
+
+	}
+
 }
