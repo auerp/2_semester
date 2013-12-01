b75e3f03e5450a1edaa9aded8d255d863a34d0c1
Patric Auer <patric.auer@googlemail.com>
2013-12-01 12:32:20 +0100

    KartenMischer als Interface hinzugefügt und in Klasse KartenDeck implementiert. Oder gehört es umgekehrt?

--- Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/KartenDeck.java
+++ Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/KartenDeck.java
@@ -1,5 +1,10 @@
 package de.hs_ma.ws_13.tpe_04_uib.auer_baummann_bucnev.generics;
 
-public class KartenDeck {
+public class KartenDeck implements KartenMischer {
 
+	// Methoden
+	// Karten mischen
+	public int mischeKarten(){
+		return ...;
+	}
 }
---
+++ Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/KartenMischer.java
@@ -0,0 +1,9 @@
+package de.hs_ma.ws_13.tpe_04_uib.auer_baummann_bucnev.generics;
+
+public interface KartenMischer {
+
+	// dient als zentraler Kartenmischer im Casino. Mischt die Karten des
+	// Kartendecks
+
+	public int mischeKarten();
+}
