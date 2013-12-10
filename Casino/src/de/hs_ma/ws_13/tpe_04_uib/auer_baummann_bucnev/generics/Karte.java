53dd2dbc06444eb853925688b2297e24a032c12d
Patric Auer <patric.auer@googlemail.com>
2013-12-10 20:06:20 +0100

    Klasse Karte geändert: Getter Methoden hinzugefügt für die Farbe und den Wert einer Karte mit Hilfe der Enum Klassen

--- Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Karte.java
+++ Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Karte.java
@@ -6,91 +6,93 @@ import enum_klassen.EnumKartenTypen;
 import enum_klassen.EnumKarten;
 
 public class Karte {
-	
-	private int farbeValue, wertValue;
-	  private String farbe[] = {"Herz", "Pik", "Karo", "Kreuz"};
-	  private String wert[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Bube", "Dame", "König","As"};
-	 
-	  private String deck [][] = new String[3][13];
 
-	  public Karte(){
-		  
-	  }
-	  
-	  protected HashMap<String, String> alleKarten = new HashMap<String, String>();
-	  
-	  
-	  
-	  public void kartenHinzufuegen(){
-		
-		  
-		  for(int i=0; i<farbe.length;i++){
-			  for(int k=0; k< wert.length; k++){
-				  deck[i][k]= farbe[i]+" " + wert[k];
-				  alleKarten.put(deck[i][k], wert[k]);
-				 
-//				  System.out.println(deck[i][k]);
-//				 System.out.println(alleKarten.get(deck[i][k]));
-				
-			  }
-		  }
-		 
-	  }
-	  
-//	  public Karte(int farbe, int wert)
-//	  {
-//		 farbeValue = farbe;
-//		 wertValue = wert;
-//	  }
+	// private int farbeValue, wertValue;
+	// private String farbe[] = {"Herz", "Pik", "Karo", "Kreuz"};
+	// private String wert[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10",
+	// "Bube", "Dame", "König","As"};
+	//
 
-	  public String convertToString(int farben){
-	    return farbe[farben];
-	  }
+	private EnumKarten wert; // Werte der Karten
+	private EnumKartenTypen typ; // Zeichen bzw. Farbe der Karten
 
-	  public void setFarbe(int farben){
-		  farbeValue = farben;
-	  }
-	  
-//	  public String getFarbe(){
-//		  for (int i=0; i<= farbe.length; i++){
-//			  System.out.println(farbe[i]);
-//			  return farbe[i];
-//			  
-//		  }
-//		  return "Hallo";
-//	  }
+	private String deck[][] = new String[3][13];
 
-	  public void setSuit(int wert){
-		  wertValue = wert;
-	  }
-
-	  public int getRank(){
-	    return farbeValue;
-	  }
-
-	  public int getSuit(){
-	    return wertValue;
-	  }
-
-	  public String toString(){
-	    String name = farbe[farbeValue] + " of " + wert[wertValue];
-	    return name;
-	  }
+	/**
+	 * Konstruktor zur Erzeugung der Karte
+	 * 
+	 * @param wert
+	 *            Wert den die KArte enthaelt
+	 * @param typ
+	 *            Farbe den die Karte enthaelt
+	 */
+	public Karte(EnumKarten wert, EnumKartenTypen typ) {
+		this.wert = wert;
+		this.typ = typ;
 	}
 
-//    private EnumKartenTypen farbe;
-//    private EnumKarten wert;
-//    
-//    public Karte(EnumKartenTypen farbe, EnumKarten wert){
-//            this.farbe = farbe;
-//            this.wert = wert;
-//    }
-//
-//    public EnumKartenTypen getFarbe() {
-//            return farbe;
-//    }
-//
-//    public EnumKarten getWert() {
-//            return wert;
-//    }
-//}
+	protected HashMap<String, String> alleKarten = new HashMap<String, String>();
+
+	public void kartenHinzufuegen() {
+
+		for (int i = 0; i < farbe.length; i++) {
+			for (int k = 0; k < wert.length; k++) {
+				deck[i][k] = farbe[i] + " " + wert[k];
+				alleKarten.put(deck[i][k], wert[k]);
+
+				// System.out.println(deck[i][k]);
+				// System.out.println(alleKarten.get(deck[i][k]));
+
+			}
+		}
+
+	}
+
+	// public String convertToString(int farben) {
+	// return farbe[farben];
+	// }
+
+	/**
+	 * Methode die die Farbe der Karte liefert
+	 * 
+	 * @return typ liefert die Farbe der Karte zurueck
+	 */
+	public EnumKartenTypen getTyp() {
+		return typ;
+	}
+
+	/**
+	 * Methode die den Wert der Karte liefert
+	 * 
+	 * @return wert liefert den Wert der Karte zurueck
+	 */
+	public EnumKarten getWert() {
+		return wert;
+	}
+
+	// public String getFarbe(){
+	// for (int i=0; i<= farbe.length; i++){
+	// System.out.println(farbe[i]);
+	// return farbe[i];
+	//
+	// }
+	// return "Hallo";
+	// }
+
+	// public void setSuit(int wert) {
+	// wertValue = wert;
+	// }
+	//
+	// public int getRank() {
+	// return farbeValue;
+	// }
+	//
+	// public int getSuit() {
+	// return wertValue;
+	// }
+
+	public String toString() {
+		String name = farbe[farbeValue] + " of " + wert[wertValue];
+		return name;
+	}
+}
