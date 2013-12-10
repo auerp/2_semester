71839cf21db6c96eccaf7c6b8b478aa590354a97
Patric Auer <patric.auer@googlemail.com>
2013-12-10 19:47:35 +0100

    Klasse Spieler geändert. Möchte ID autmatisch vergeben damit dieser bei einem neuen spieler automatisch hochgezählt wird

--- Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Spieler.java
+++ Casino/src/de/hs_ma/ws_13/tpe_04_uib/auer_baummann_bucnev/generics/Spieler.java
@@ -2,52 +2,73 @@ package de.hs_ma.ws_13.tpe_04_uib.auer_baummann_bucnev.generics;
 
 public class Spieler {
 
-	// public enum Teilnehmer {
-	// Dealer, Spieler;
-	// }
-
 	private String name;
 	private String id;
 	private int vermoegen;
 
-	//Konstruktor
-	Spieler(String name, String id, int vermoegen) {
-		this.name = name;
-		this.id = id;
-		// this.vermoegen = vermoegen;
-		setVermoegen(vermoegen);
-		
-	}
-	
-	//Geld setzen
-	public void setzen(int betrag) {
-        this.vermoegen =  this.vermoegen -betrag;
-        }
-	
-	//Geld gewonnen
-	public void gewonnen(int betrag){
-        this.vermoegen = this.vermoegen+ betrag;
-    }
+	int intId = Integer.parseInt(id); // String wird in Integer umgewandelt
 
-	// Getter Methoden
-	// getName
+	// Integer integerId = new Integer(stringId);
+
+	/**
+	 * Konstruktor zur Erzeugung eines Spielers
+	 * 
+	 * @param name
+	 *            Name eines Spielers
+	 * @param id
+	 *            ID eines Spielers
+	 * @param vermoegen
+	 *            Das Vermoegen eines Spielers
+	 */
+	public Spieler(String name, String id, int vermoegen) {
+		this.name = name;
+		this.vermoegen = vermoegen;
+		// setVermoegen(vermoegen);
+		this.intId++;
+		this.id = intId.toString();
+	}
+
+	// Geld setzen
+	public void setzen(int betrag) {
+		this.vermoegen = this.vermoegen - betrag;
+	}
+
+	/**
+	 * Methode um den Betrag zu erhoehen wenn ein Spieler gewonnen hat
+	 * 
+	 * @param betrag
+	 *            Betrag um welchen das Vermoegen erhoeht wird
+	 */
+	public void gewonnen(int betrag) {
+		this.vermoegen = this.vermoegen + betrag;
+	}
+
+	/**
+	 * Methode, die den Name liefert
+	 * 
+	 * @return name liefert den Name des Spielers
+	 */
 	public String getName() {
 		return this.name;
 	}
 
-	// getID
+	/**
+	 * Methode, um die ID zu ermitteln
+	 * 
+	 * @return id liefert die ID des Spielers
+	 */
 	public String getId() {
-		return this.id;
+		return this.stringId;
 	}
 
-	// getVermoegen
-	public int getVermoegen() {
-		return this.vermoegen;
-	}
-
-	// Setter Methoden
-	// setVermoegen
-	public void setVermoegen(int vermoegen) {
-		this.vermoegen = vermoegen;
-	}
+	// // getVermoegen
+	// public int getVermoegen() {
+	// return this.vermoegen;
+	// }
+	//
+	// // Setter Methoden
+	// // setVermoegen
+	// public void setVermoegen(int vermoegen) {
+	// this.vermoegen = vermoegen;
+	// }
 }
